b3c516d134207aae1620321c29cb57a60b6453c8
mlanders4 <mlanders4@huskers.unl.edu>
2023-08-28 14:53:32 -0500

    added my major to the output

--- hello.c
+++ hello.c
@@ -1,5 +1,19 @@
+/*
+Author: Matthew Landers
+
+Date: 2023/08/24
+
+Class: CSCE 155E
+
+Description: Prints my name and my major
+*/
+
+
+
 int main(int argc, char **argv) {
 
+    //prints out my name and then prints out my major.
+    printf("Hello World, my name is Matthew Landers!\n");
     printf("My major is computer science!\n");
 
     return 0;
